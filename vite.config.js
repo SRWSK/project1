e57a7c505016e8c3df63cb3eb94ac0227d684fe5
SRWSK <69319010016@sbt.ac.th>
2026-07-14 14:50:57 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react()
-  ],
-
-  // ชื่อ Repository GitHub
-  base: "/saraburi-food-guide/",
+  plugins: [react()],
+  base: "/saraburi-food-guide/", // เปลี่ยนเป็นชื่อ Repository ของคุณ
 });
